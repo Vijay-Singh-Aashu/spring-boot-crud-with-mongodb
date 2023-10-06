d89359be13afaa7469b9672cb5fd9cd992185e69
Vijay Singh <vijaysinghaashu@gmail.com>
2023-10-06 11:20:15 +0530

    spring-boot-with-mongodb-crud-api-starts

---
+++ src/test/java/com/excellence/spring/data/mongodb/SpringBootDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.spring.data.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
